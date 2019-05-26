3149db0b29d3cdbc20f29fa4190e7184d2cc3189
isidoreduc <isidoreducasse79@gmail.com>
2019-05-26 16:17:35 +0200

    recipe list

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Pizza Margherita',
+      'A simple, but delicious pizza',
+      'https://i.ytimg.com/vi/K3Ceg_QB6e0/maxresdefault.jpg'
+    ),
+    new Recipe(
+      'Pizza Pesce',
+      'Fish Galore in a wodden burning oven made bread',
+      'http://www.latavernadegliassi.com/wp-content/uploads/2016/04/pizza-pesce.jpg'
+    )
+  ];
 
   constructor() {}
 
